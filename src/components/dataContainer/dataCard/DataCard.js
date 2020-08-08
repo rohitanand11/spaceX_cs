b6f5dc327b5ad99007048be4eb6d03022f0b7497
Rohit Anand <rohitanand3456@gmail.com>
2020-08-09 01:31:31 +0530

    added the description wanted and passed on     the result from the api.

--- src/components/dataContainer/dataCard/DataCard.js
+++ src/components/dataContainer/dataCard/DataCard.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import Classes from './DataCard.module.scss';
-import ImageContainer from './ImageContainer/ImageContainer';
-import Description from './description/Description';
+import React from "react";
+import Classes from "./DataCard.module.scss";
+import ImageContainer from "./ImageContainer/ImageContainer";
+import Description from "./description/Description";
 
-const DataCard = ({data}) => {
-    console.log(`here datacard-> ${data}`);
-    return (
-        <div className={Classes.DataCard}>
-            <ImageContainer imageUrl={data.links.mission_patch_small}/>
-            <Description />
-        </div>
-    )
-}
+const DataCard = ({ data }) => {
+    const suc_launch = data.launch_success?'true':'false';
+   // const SuccessfulLanding = data.rocket.first_stage.cores[1].land_success !==undefined && data.rocket.first_stage.cores[1].land_success ==='true' ? 'true' : 'false'
+
+  return (
+    <div className={Classes.DataCard}>
+      <ImageContainer imageUrl={data.links.mission_patch_small} />
+      <Description
+        missionIds={data.mission_id}
+        launch_year = {data.launch_year}
+        suc_lauch={suc_launch}
+        // SuccessfulLanding={SuccessfulLanding}
+      />
+    </div>
+  );
+};
 
 export default DataCard;
--- src/components/dataContainer/dataCard/description/Description.js
+++ src/components/dataContainer/dataCard/description/Description.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import Classes from './Description.module.scss';
 
-const Description = () => {
+const Description = ({missionIds,launch_year,suc_lauch}) => {
     return (
         <div className={Classes.Description}>
-            hey
-            hello
-            heya
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Mission Id's : </th>
+                        <td>{missionIds}</td>
+                    </tr>
+
+                    <tr>
+                        <th>Launch Year : </th>
+                        {console.log(launch_year)}
+                        <td>{launch_year}</td>
+                    </tr>
+
+                    <tr>
+                        <th>Successful Launch : </th>
+                        <td>{suc_lauch}</td>
+                    </tr>
+                 </tbody>
+            </table>
         </div>
-    )
-}
+    );
+};
 
 export default Description;
